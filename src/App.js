114300da8937e83f856c3e5f83c3263b6de25d4b
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-01-09 16:05:22 +0700

    Get data People and Filter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import logo from './logo.svg';
+
+function searchingFor (term){
+  return function(x){
+    return x.name.toLowerCase().includes(term.toLowerCase()) || !term; 
+  }
+}
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      people: [],
+      term : ''
+    }
+    this.searchHandler = this.searchHandler.bind(this);
+  }
+  
+  searchHandler (event) {
+    this.setState({
+      term : event.target.value
+    })
+  }
+
+  getVideos = () => {
+    axios.get('https://swapi.co/api/people')
+      .then(res => {
+        this.setState({people: res.data.results})
+      }).catch(err => {
+        console.log(err)
+      })
+  }
+
+  componentDidMount(){
+    this.getVideos();
+  }
+
+
   render() {
+    const {term, people} = this.state
     return (
       <div className="App">
-        <header className="App-header">
+
+<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +57,21 @@ class App extends Component {
           >
             Learn React
           </a>
+          
+          <label>Cari</label>
+          <form>
+            <input
+              type="text"
+              onChange={this.searchHandler}
+            />
+          </form>
+          {
+            people.filter(searchingFor(term)).map(people =>
+              <div key={people.id}>
+                <h1>{people.name}</h1>
+              </div>
+            )
+          }
         </header>
       </div>
     );
